cbe1eaf3980faac69a674536efe2a09abf278386
Mopahta <53401187+Mopahta@users.noreply.github.com>
2022-11-30 22:26:34 +0300

    now creating new thread running function

--- src/vowel-conduit.cpp
+++ src/vowel-conduit.cpp
@@ -1,8 +1,19 @@
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-extern "C" __declspec(dllexport) void __stdcall vowelMain();
+extern "C" __declspec(dllexport) BOOL vowelMain();
 
-extern "C" __declspec(dllexport) void __stdcall vowelMain() {
+DWORD WINAPI vowelThread(LPVOID params) {
     MessageBoxA(NULL, "Hi", "fkdslhf", NULL);
+    int c = 2+2;
+    int a = 5;
+    int b = c * a;
 
+    return 0;
 }
+
+extern "C" __declspec(dllexport) BOOL vowelMain() {
+    CreateThread(NULL, NULL, vowelThread, NULL, NULL, NULL);
+    return TRUE;
+}
+

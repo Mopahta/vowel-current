732f6072b5d14d7e796876464f7de4b205548ea3
Mopahta <53401187+Mopahta@users.noreply.github.com>
2022-12-10 13:54:10 +0300

    fixed vector

--- src/structs/vector.h
+++ src/structs/vector.h
@@ -4,26 +4,27 @@
 #define _VECTOR3D
 
 struct Vector3 {
+    float x, y, z;
     constexpr Vector3 (float x = 0.f, float y = 0.f, float z = 0.f) :
         x(x), y(y), z(z) {}
 
-    constexpr const Vector3& operator-(const Vector3& other) const noexcept {
-        return Vector3{x - other.x, y - other.y, z - other.z};
+    constexpr const Vector3 operator-(const Vector3& other) const noexcept {
+        return Vector3(x - other.x, y - other.y, z - other.z);
     }
 
-    constexpr const Vector3& operator+(const Vector3& other) const noexcept {
-        return Vector3{x + other.x, y + other.y, z + other.z};
+    constexpr const Vector3 operator+(const Vector3& other) const noexcept {
+        return Vector3(x + other.x, y + other.y, z + other.z);
     }
 
-    constexpr const Vector3& operator/(const float num) const noexcept {
-        return Vector3{x / num, y / num, z / num};
+    constexpr const Vector3 operator/(const float num) const noexcept {
+        return Vector3(x / num, y / num, z / num);
     }
 
-    constexpr const Vector3& operator*(const float num) const noexcept {
-        return Vector3{x * num, y * num, z * num};
+    constexpr const Vector3 operator*(const float num) const noexcept {
+        return Vector3(x * num, y * num, z * num);
     }
 
-    constexpr const Vector3& toAngle() const noexcept {
+    constexpr const Vector3 toAngle() const noexcept {
         return Vector3 {
             std::atan2(-z , std::hypot(x, y)) * (180.0f / M_PI),
             std::atan2(y, x) * (180.0f / M_PI),
@@ -35,7 +36,6 @@ struct Vector3 {
         return x == 0.f && y == 0.f && z == 0.f;
     }
 
-    float x, y, z;
 };
 
 
